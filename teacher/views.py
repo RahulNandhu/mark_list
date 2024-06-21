e685f615e886df74309d9d3566c48e58a6145e41
RahulNandhu <rahulpnandhu111@gmail.com>
2024-06-21 20:45:17 +0530

    Top Students added

--- teacher/views.py
+++ teacher/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
+from .models import Marks
+from django.db.models import F, ExpressionWrapper, IntegerField
+
 
 # Create your views here.
 
 def Home(request):
-    return render(request,template_name='Home.html')
+    total_expression = (
+            F('Language_1') + F('Language_2') + F('Maths') +
+            F('Physics') + F('Chemistry') + F('Statistics') +
+            F('Computer_Science')
+    )
+
+    Top_3 = Marks.objects.annotate(
+        total_marks=ExpressionWrapper(total_expression, output_field=IntegerField())
+    ).order_by('-total_marks')  # Sorted in descending order by total marks
+
+    if len(Top_3)>3:
+        Top_3=Top_3[3]
+
+    context={'T3':Top_3}
+    return render(request,template_name='Home.html',context=context)
