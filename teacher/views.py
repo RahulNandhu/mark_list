65c0551d5dde57909580af1941c1aa2a8c42999f
RahulNandhu <rahulpnandhu111@gmail.com>
2024-06-23 13:54:43 +0530

    Semester wise exam Result enabled

--- teacher/views.py
+++ teacher/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from .models import Marks
-from django.db.models import F, ExpressionWrapper, IntegerField
+from .models import Marks,Semester
+from django.db.models import F, ExpressionWrapper, IntegerField,Sum
 
 
 # Create your views here.
@@ -12,18 +12,75 @@ def Home(request):
             F('Computer_Science')
     )
 
-    Top_3 = Marks.objects.annotate(
+    semester=Semester.objects.get(name='Semester 1')
+    mark_queryset = Marks.objects.filter(semester=semester)
+
+    annotated_marks = mark_queryset.annotate(
         total_marks=ExpressionWrapper(total_expression, output_field=IntegerField())
-    ).order_by('-total_marks')  # Sorted in descending order by total marks
+    )
 
-    if len(Top_3)>3:
-        Top_3=Top_3[3]
+    # Aggregate and sort by total_marks for each student and semester
+    Top = (
+        annotated_marks.values('student__id', 'student__Name', 'semester__id', 'semester__name')
+            .annotate(total_marks=Sum('total_marks'))
+            .order_by('-total_marks')
+    )
+
+    # Top 3 students in Maths
+    maths = (
+        annotated_marks.values('student__id', 'student__Name', 'semester__id', 'semester__name')
+            .annotate(total_marks=Sum('Maths'))
+            .order_by('-total_marks')
+    )
+
+    # Top 3 students in Physics
+    physics = (
+        annotated_marks.values('student__id', 'student__Name', 'semester__id', 'semester__name')
+            .annotate(total_marks=Sum('Physics'))
+            .order_by('-total_marks')
+    )
 
 
-    Maths=Marks.objects.all().order_by('-Maths')
 
-    if len(Maths) >3:
-        Maths=Maths[3]
+    if request.method=='POST':
+        sem=request.POST['Sem']
+        # print(sem)
+        semester=Semester.objects.get(id=sem)
+        mark_queryset=Marks.objects.filter(semester=semester)
 
-    context={'T3':Top_3,'Maths':Maths}
+        annotated_marks = mark_queryset.annotate(
+            total_marks=ExpressionWrapper(total_expression, output_field=IntegerField())
+        )
+
+        # Aggregate and sort by total_marks for each student and semester
+        Top = (
+            annotated_marks.values('student__id', 'student__Name', 'semester__id', 'semester__name')
+                .annotate(total_marks=Sum('total_marks'))
+                .order_by('-total_marks')
+        )
+
+        # Top 3 students in Maths
+        maths = (
+            annotated_marks.values('student__id', 'student__Name', 'semester__id', 'semester__name')
+                .annotate(total_marks=Sum('Maths'))
+                .order_by('-total_marks')
+        )
+
+        # Top 3 students in Physics
+        physics = (
+            annotated_marks.values('student__id', 'student__Name', 'semester__id', 'semester__name')
+                .annotate(total_marks=Sum('Physics'))
+                .order_by('-total_marks')
+        )
+
+        context = {
+            'T3': Top,
+            'Maths': maths,
+            'Physics': physics,
+            'semester':semester
+        }
+
+        return render(request, 'Home.html', context)
+
+    context={'T3':Top,'Maths':maths,'Physics':physics,'semester':'Semester 1'}
     return render(request,template_name='Home.html',context=context)
