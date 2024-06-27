5d93de1f6d2ee574677e0833c7bf8a901f093933
RahulNandhu <rahulpnandhu111@gmail.com>
2024-06-27 21:28:42 +0530

    search option enabled

--- student/views.py
+++ student/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
+from teacher.models import Marks,Students
 
 # Create your views here.
 
 def Search_student(request):
 
     if request.method=='POST':
-        return render(request, template_name='student.html')
-    return render(request,template_name='home.html')
+        search_key=request.POST['search']
+
+        if search_key !='':
+            try:
+                st=Students.objects.get(Sid=search_key)
+                msg=''
+            except:
+                st=''
+                msg='Enter Valid Student Id'
+            return render(request, template_name='student.html',context={'msg':msg,'st':st})
+        else:
+            return render(request,template_name='student.html',context={'msg':'Enter Student Id'})
