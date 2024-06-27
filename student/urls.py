ee50826468d1d9f1f859a43d6a7b375e692a237d
RahulNandhu <rahulpnandhu111@gmail.com>
2024-06-27 21:28:11 +0530

    search option enabled

--- student/urls.py
+++ student/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from . import views
 
 app_name='student'
 
 urlpatterns=[
-
+    path('',views.Search_student,name="search_student"),
 ]
--- student/views.py
+++ student/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def Search_student(request):
+
+    if request.method=='POST':
+        return render(request, template_name='student.html')
+    return render(request,template_name='home.html')
